eb80d2d88a35d028fcd00acb566650d334cd1596
Lucas Leandro Costa Lacerda <lucas.lacerdabh@gmail.com>
2018-08-31 16:32:25 -0300

    Finish implementations

---
+++ src/implementations/complex.polar.cpp
@@ -0,0 +1,138 @@
+#include <cmath>
+
+#include "../interfaces/complex.h"
+
+#pragma region[Constructors]
+
+Complex::Complex(void)
+{
+    _realNumber = 0;
+    _imaginaryNumber = 0;
+};
+
+Complex::Complex(double realNumber)
+{
+    _realNumber = realNumber;
+    _imaginaryNumber = 0;
+};
+
+Complex::Complex(double realNumber, double imaginaryNumber)
+{
+    _realNumber = realNumber;
+    _imaginaryNumber = imaginaryNumber;
+};
+
+#pragma endregion
+
+#pragma region[Properties]
+
+double Complex::GetRealNumber()
+{
+    return _realNumber;
+};
+
+double Complex::GetImaginaryNumber()
+{
+    return _imaginaryNumber;
+};
+
+#pragma endregion
+
+#pragma region[Methods]
+
+double Complex::Module()
+{
+    std::fabs(GetRealNumber());
+};
+
+Complex Complex::Conjugated()
+{
+    Complex conjugated(GetRealNumber(), -GetImaginaryNumber());
+    return conjugated;
+};
+
+Complex Complex::Invert()
+{
+    Complex resultComplex(1/GetRealNumber(), -GetImaginaryNumber());
+    return resultComplex;
+};
+
+#pragma endregion
+
+#pragma region[Operators]
+
+bool Complex::operator==(Complex x)
+{
+    if (GetRealNumber() == x.GetRealNumber() && GetImaginaryNumber() == x.GetImaginaryNumber())
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+};
+
+bool Complex::operator=(Complex x)
+{
+    _realNumber = x.GetRealNumber();
+    _imaginaryNumber = x.GetImaginaryNumber();
+    return true;
+};
+
+Complex Complex::operator-()
+{
+    Complex oppositeComplex(-GetRealNumber(), GetImaginaryNumber() + 180);
+    return oppositeComplex;
+};
+
+Complex Complex::operator+(Complex complexToBeAdd)
+{
+    double realNumberAsEuclidean = cos(GetImaginaryNumber()) * Module();
+    double imaginaryNumberAsEuclidean = sin(GetImaginaryNumber()) * Module();
+
+    double complexToBeAddRealNumberAsEuclidean = cos(complexToBeAdd.GetImaginaryNumber()) * complexToBeAdd.Module();
+    double complexToBeAddImaginaryNumberAsEuclidean = sin(complexToBeAdd.GetImaginaryNumber()) * complexToBeAdd.Module();
+
+    double realNumberResultAsPolar = sqrt(pow(imaginaryNumberAsEuclidean + complexToBeAddImaginaryNumberAsEuclidean,2) + pow(realNumberAsEuclidean + complexToBeAddRealNumberAsEuclidean, 2));
+    double imaginaryNumberResultAsPolar = atan((complexToBeAddImaginaryNumberAsEuclidean + imaginaryNumberAsEuclidean)/(complexToBeAddRealNumberAsEuclidean + realNumberAsEuclidean));
+    
+    Complex result(realNumberResultAsPolar, imaginaryNumberResultAsPolar);
+
+    return result;
+};
+
+Complex Complex::operator-(Complex complexToBeSubtract)
+{
+    double realNumberAsEuclidean = cos(GetImaginaryNumber()) * Module();
+    double imaginaryNumberAsEuclidean = sin(GetImaginaryNumber()) * Module();
+
+    double complexToBeSubtractRealNumberAsEuclidean = cos(complexToBeSubtract.GetImaginaryNumber()) * complexToBeSubtract.Module();
+    double complexToBeSubtractImaginaryNumberAsEuclidean = sin(complexToBeSubtract.GetImaginaryNumber()) * complexToBeSubtract.Module();
+
+    double realNumberResultAsPolar = sqrt(pow(imaginaryNumberAsEuclidean - complexToBeSubtractImaginaryNumberAsEuclidean,2) + pow(realNumberAsEuclidean - complexToBeSubtractRealNumberAsEuclidean, 2));
+    double imaginaryNumberResultAsPolar = atan((complexToBeSubtractImaginaryNumberAsEuclidean - imaginaryNumberAsEuclidean)/(complexToBeSubtractRealNumberAsEuclidean - realNumberAsEuclidean));
+    
+    Complex result(realNumberResultAsPolar, imaginaryNumberResultAsPolar);
+
+    return result;
+};
+
+Complex Complex::operator*(Complex complexToBeMultiplicate)
+{
+    double realResult = Module() * complexToBeMultiplicate.Module();
+    double imaginaryResult = GetImaginaryNumber() + complexToBeMultiplicate.GetImaginaryNumber();
+    Complex resultComplex(realResult, imaginaryResult);
+    return resultComplex;
+};
+
+Complex Complex::operator/(Complex complexToBeDivided)
+{
+    double realResult = Module() / complexToBeDivided.Module();
+    double imaginaryResult = GetImaginaryNumber() - complexToBeDivided.GetImaginaryNumber();
+    Complex resultComplex(realResult, imaginaryResult);
+    return resultComplex;
+};
+
+#pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 #include <cmath>
 
 #include "./interfaces/complex.h"
-#include "./implementations/complex.euclidean.h"
-#include "./implementations/complex.polar.h"
-
 
 int main() {
     return 0;
